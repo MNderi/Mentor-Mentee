48c012eea0da4ddf608ec716044d2963b0bc7b82
Marthar Nderitu <97471898+MNderi@users.noreply.github.com>
2023-12-19 20:24:30 +0000

    Update db.js

--- coachApp/model/db.js
+++ coachApp/model/db.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI; // Use process.env to access environment variables
-    if (!mongoURI) {
+    const mongo_URI = process.env.MONGO_URI; // Use process.env to access environment variables
+    if (!mongo_URI) {
       throw new Error('MongoDB URI is not defined in the environment variables.');
     }
 
-    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(mongo_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
